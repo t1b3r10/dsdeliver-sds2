0f645071bf85a1e7799d26584166bd1a4f0fdd16
t1b3r10 <58698163+t1b3r10@users.noreply.github.com>
2021-01-06 20:47:33 -0300

    GET /orders
    Criar endpoints
    - [GET] /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -102,6 +102,10 @@ public class Order implements Serializable {
 		this.status = status;
 	}
 
+	public Set<Product> getProducts() {
+		return products;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
