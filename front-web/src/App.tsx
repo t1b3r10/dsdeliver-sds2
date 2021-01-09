b76fdde04a02da9e8eb87061d5c4005a4edf0392
t1b3r10 <58698163+t1b3r10@users.noreply.github.com>
2021-01-08 21:52:04 -0300

    Add home page
    Página principal da home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './NavBar';
 
 function App() {
   return (
       <div>
           <Navbar />
+          <Home />
       </div>
   );
 }
